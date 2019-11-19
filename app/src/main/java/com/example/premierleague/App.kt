56d613a9e20f453d010ced61757ed36f9ff47bd5
ziyadkhalil <ziyadhkhalil@gmail.com>
2019-11-19 16:26:38 +0200

    Added App class to initiate Koin

--- app/src/main/java/com/example/premierleague/App.kt
+++ app/src/main/java/com/example/premierleague/App.kt
@@ -1,7 +1,22 @@
 package com.example.premierleague
 
+import android.app.Application
+import com.example.premierleague.di.retrofitModule
+import com.example.premierleague.di.roomModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+
 /**
  * Created by Ziyad on Nov, 2019
  */
-class App {
+class App: Application() {
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidLogger()
+            androidContext(this@App)
+            modules(listOf(retrofitModule, roomModule))
+        }
+    }
 }
