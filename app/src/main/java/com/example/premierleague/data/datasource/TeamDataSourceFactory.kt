805754d0fc5c60e4b4fecd465deef8ce6353bbb2
ziyadkhalil <ziyadhkhalil@gmail.com>
2019-11-20 05:16:21 +0200

    Player dao, data source, data source factory

--- app/src/main/java/com/example/premierleague/data/datasource/TeamDataSourceFactory.kt
+++ app/src/main/java/com/example/premierleague/data/datasource/TeamDataSourceFactory.kt
@@ -3,18 +3,25 @@ package com.example.premierleague.data.datasource
 import androidx.lifecycle.MutableLiveData
 import androidx.paging.DataSource
 import androidx.paging.RxPagedListBuilder
+import com.example.premierleague.data.local.DatabaseHandler
 import com.example.premierleague.data.model.Team
+import com.example.premierleague.data.remote.ApiService
 import io.reactivex.Observable
+import io.reactivex.disposables.CompositeDisposable
 
 /**
  * Created by Ziyad on Nov, 2019
  */
-class TeamDataSourceFactory(private val dataManager: DataManager): DataSource.Factory<Int, Team>() {
+class TeamDataSourceFactory(private val database: DatabaseHandler, private val api: ApiService): DataSource.Factory<Int, Team>() {
     private val sourceObservableData = MutableLiveData<TeamsDataSource>()
     private lateinit var latestSource: TeamsDataSource
+    private var compositeDisposable = CompositeDisposable()
     override fun create(): DataSource<Int, Team> {
-        latestSource = TeamsDataSource(dataManager)
+        compositeDisposable.dispose()
+        compositeDisposable = CompositeDisposable()
+        latestSource = TeamsDataSource(database, api, compositeDisposable)
         sourceObservableData.postValue(latestSource)
         return latestSource
     }
+
 }
---
+++ app/src/main/java/com/example/premierleague/data/local/PlayersDao.kt
@@ -0,0 +1,2 @@
+package com.example.premierleague.data.local 
+
